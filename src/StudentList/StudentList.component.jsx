df5477658c6bf6d2a4c259d9f0f95ff08923f170
mauricejthomas <mauricejthomas@gmail.com>
2021-03-20 14:29:45 -0400

    React Practice

--- src/StudentList/StudentList.component.jsx
+++ src/StudentList/StudentList.component.jsx
@@ -1,71 +1,85 @@
 import React, {Component} from "react";
 
+import FavoriteCandy from '../FavoriteCandy/FavoriteCandy.component'
 class StudentList extends Component{ 
     constructor(){
         super()
         this.state={
              StudentData : [
-            {
-              id: 1,
-              name: "Eddy",
-              color: "chartreuse",
-              school: "Nebula Academy",
-            },
-            {
-              id: 2,
-              name: "Chelsea",
-              color: "purple",
-              school: "Nebula Academy",
-            },
-            {
-              id: 3,
-              name: "Tauseef",
-              color: "orange",
-              school: "Nebula Academy",
-            },
-            {
-              id: 4,
-              name: "Maurice",
-              color: "red",
-              school: "Nebula Academy",
-            },
-            {
-              id: 5,
-              name: "D'aja",
-              color: "blue",
-              school: "Nebula Academy",
-            },
-            {
-              id: 6,
-              name: "Jasmine",
-              color: "pink",
-              school: "Nebula Academy",
-            },
-            {
-              id: 7,
-              name: "Anitria",
-              color: "blue",
-              school: "Nebula Academy",
-            },
+              {
+                id: 1,
+                name: "Eddy",
+                color: "chartreuse",
+                school: "Nebula Academy",
+                animal: "cat",
+                candy: "skittles",
+              },
+              {
+                id: 2,
+                name: "Chelsea",
+                color: "purple",
+                school: "Nebula Academy",
+                animal: "dinosaurs",
+                candy: "kit-kat",
+              },
+              {
+                id: 3,
+                name: "Tauseef",
+                color: "orange",
+                school: "Nebula Academy",
+                animal: "rabbit",
+                candy: "mini snickers",
+              },
+              {
+                id: 4,
+                name: "Maurice",
+                color: "red",
+                school: "Nebula Academy",
+                animal: "dog",
+                candy: "reese's",
+              },
+              {
+                id: 5,
+                name: "D'aja",
+                color: "blue",
+                school: "Nebula Academy",
+                animal: "elephants",
+                candy: "twix",
+              },
+              {
+                id: 6,
+                name: "Jasmine",
+                color: "pink",
+                school: "Nebula Academy",
+                animal: "bear",
+                candy: "milky way",
+              },
+              {
+                id: 7,
+                name: "Anitria",
+                color: "blue",
+                school: "Nebula Academy",
+                animal: "bear",
+                candy: "snickers",
+              },
           ]
 
         }
     }
     render(){
         return(
-            <div>
+          <div>
             {this.state.StudentData.map(student => (
-    <div className="studentCard">
-      <h1>Hi {student.name}</h1>
-      <p>
-        {student.name}'s favorite color is  {student.color}
-      </p>
-    </div>
-  ))}
-            </div>
+              <div className="studentCard">
+                <h1>Hi {student.name}</h1>
+                <p>
+                {student.name} is a {student.school} student
+                </p>
+                < FavoriteCandy CandyLand={this.state}/>
+              </div>
+            ))}
+          </div>
         )
-    
     }
-
 }
 export default StudentList
